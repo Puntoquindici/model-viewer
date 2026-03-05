c2ef5c4717c199e07b795544c0e437edde83b94b
Riccardo Arena <riccardo@puntoquindici.it>
2026-03-05 11:20:21 +0100

    FIX: hdr loading from blobs (ex. from file upload)

--- packages/model-viewer/src/three-components/TextureUtils.ts
+++ packages/model-viewer/src/three-components/TextureUtils.ts
@@ -53,8 +53,7 @@ export default class TextureUtils extends EventDispatcher {
       url: string, progressCallback: (progress: number) => void = () => {}):
       Promise<Texture> {
     try {
-      let url_data = new URL(url);
-      const isHDR: boolean = HDR_FILE_RE.test(url_data.pathname);
+      const isHDR: boolean = HDR_FILE_RE.test(url);
       const loader = isHDR ? hdrLoader : ldrLoader;
       const texture: Texture = await new Promise<Texture>(
           (resolve, reject) => loader.load(
